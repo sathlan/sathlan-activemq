89d6b2467ff907d6bd81d7665973bf47ce8af59b
sathlan <mypublicaddress-code@ymail.com>
2012-07-13 16:39:38 +0200

    Cleanup.

--- manifests/debian/install.pp
+++ manifests/debian/install.pp
@@ -3,11 +3,10 @@ class activemq::debian::install(){
   case $::lsbdistcodename {
     "squeeze": {
       package { 'openjdk-6-jre-headless':}
-      class { 'apt::debian::testing': } # provided by stdlib
       apt::force { 'activemq':
         release => 'testing',
         version => '5.6.0+dfsg-1',
-        require => [Class['Apt::Debian::Testing'], Package['openjdk-6-jre-headless']]
+        require => Package['openjdk-6-jre-headless']
       }
     }
 
